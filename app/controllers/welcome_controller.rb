6fe29f4ba9813d2be76f3df2cef244bf9491010f
Macx <k293590509@gmail.com>
2017-02-16 19:39:48 +0800

    create groups index eit exit quit exit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "早上好！孩子！"
+    flash[:alert] = "Wanan!Good night!"
+    flash[:warning] = "Bad!Later!"
+  end
+
 end
